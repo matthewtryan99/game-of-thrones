6997289e5778a1b4e707fe978ba56dadad537c63
Matt Ryan <matthewtryan99@gmail.com>
2020-02-18 11:24:03 -0600

    character and house display change

--- js/game-of-thrones.js
+++ js/game-of-thrones.js
@@ -1,58 +1,69 @@
-var container = document.querySelector('.container-fluid');
-var houses = []
-var characters = [];
-for(let i = 1; i < 112; i++)
+var fullCharList = [];
+var fullHouseList = [];
+var ul = document.querySelector('ul');
+var x = 0;
+var j = 0;
+var lis = [];
+
+for(let i = 1; i < 25; i++)
 {
-    fetch(`https://anapioficeandfire.com/api/houses?page=${i}&pageSize=4`)
+    fetch(`https://anapioficeandfire.com/api/houses?page=${i}&pageSize=50`)
     .then((response)=>response.json())
     .then((e)=>{
-        for(let inner = 0; inner < 4; inner++)
+        fullHouseList = [...fullHouseList, ...e]
+    })
+}
+
+for(var i = 1; i < 50; i++)
+{
+    fetch(`https://anapioficeandfire.com/api/characters?page=${i}&pageSize=50`)
+    .then((response)=>response.json())
+    .then((e)=>{
+        fullCharList = [...fullCharList, ...e]
+        for(x; x < fullCharList.length; x++){
+            let li = document.createElement('li');
+            if (fullCharList[x].name == '' && fullCharList[x].allegiances.length > 0)
+            {
+                let houseURL = fullCharList[x].allegiances[0].split('/');
+                let index = houseURL.pop();
+                li.innerHTML = `${fullCharList[x].aliases[0]}-<a href='#'data-toggle='modal' data-target='#exampleModalCenter'>${fullHouseList[index - 1].name}</a>`
+            }
+            else if(fullCharList[x].name != '' && fullCharList[x].allegiances.length > 0)
+            {
+                let houseURL = fullCharList[x].allegiances[0].split('/');
+                let index = houseURL.pop();
+                li.innerHTML = `${fullCharList[x].name}-<a href='#' data-toggle='modal' data-target='#exampleModalCenter'>${fullHouseList[index - 1].name}</a>`
+            }
+            else if(fullCharList[x].name != '')
+            {
+                li.innerHTML = `${fullCharList[x].name}`
+            }
+            else
+            {
+                li.innerHTML = `${fullCharList[x].aliases[0]}`
+            }
+            ul.append(li);
+        }
+    })
+    .then((e)=>{
+        let liList = document.querySelectorAll('li')
+        for(j; j < liList.length; j++)
         {
-            // console.log(e[inner].name);
+            liList[j].addEventListener('click', (e)=>{
+                let exampleModalHeader = document.querySelector('#exampleModalLongTitle')
+                let namesOfHouses = fullHouseList.map((e)=>{
+                    return e.name;
+                })
+                let index = namesOfHouses.indexOf(e.target.textContent)
+                exampleModalHeader.textContent = fullHouseList[index].name
+                let modalBody = document.querySelector('.modal-body')
+                let lordIndexArray = fullHouseList[index].currentLord.split('/')
+                let lordIndex = lordIndexArray.pop()
+                let lord = fullCharList[lordIndex].name
+                console.log(lord);
+                modalBody.innerHTML = `<div>Region: ${fullHouseList[index].region}</div><div>Coat of Arms: ${fullHouseList[index].coatOfArms}</div><div>Current Lord: ${fullCharList[lordIndex].name}</div>`
 
-            houses.push(e[inner].name)
-
+            })
         }
     })
 }
-for(let i = 1; i < 535; i++)
-{
-    fetch(`https://anapioficeandfire.com/api/characters?page=${i}&pageSize=4`)
-    .then((response)=>response.json())
-    .then((e)=>{
-        let div = document.createElement('div');
-        let outDiv = document.createElement('div')
-        outDiv.className = "name column align-items-center justify-content-center m-2"
-        let newArray = e.map((element)=>{
-            
-            characters.push(element);
-            
-            if(element.name == '' && element.allegiances.length > 0)
-            {
-                let houseArr = element.allegiances[0].split('/')
-                let house = houseArr.pop();
-                return `<div class="col-3 d-inline-flex">${element.aliases[0]}-<a href="#">${houses[house-1]}</a></div>`
-            }
-            else if(element.name != '' && element.allegiances.length > 0)
-            {
-                let houseArr = element.allegiances[0].split('/')
-                let house = houseArr.pop();
-                return `<div class="col-3 d-inline-flex">${element.name}-<a href="#">${houses[house-1]}</a></div>`;
-            }
-            else if(element.name != '' && element.allegiances.length == 0) 
-            {
-                return `<div class="col-3 d-inline-flex">${element.name}-No House</div>`;
-            }
-            else 
-            {
-                return `<div class="col-3 d-inline-flex">${element.aliases[0]}-No House</div>`;
-            }
-        })
-
-        div.innerHTML = newArray.join('')
-        outDiv.append(div)
-        container.append(outDiv)
-    })
-}
-
-console.log(characters);
