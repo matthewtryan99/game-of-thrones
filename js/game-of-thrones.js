78988b6f30bc12f2837e8b4b4962268aae928cf3
Matt Ryan <matthewtryan99@gmail.com>
2020-02-18 09:59:37 -0600

    Names and houses printed

--- js/game-of-thrones.js
+++ js/game-of-thrones.js
@@ -1,5 +1,6 @@
 var container = document.querySelector('.container-fluid');
 var houses = []
+var characters = [];
 for(let i = 1; i < 112; i++)
 {
     fetch(`https://anapioficeandfire.com/api/houses?page=${i}&pageSize=4`)
@@ -14,7 +15,7 @@ for(let i = 1; i < 112; i++)
         }
     })
 }
-for(let i = 1; i < 1070; i++)
+for(let i = 1; i < 535; i++)
 {
     fetch(`https://anapioficeandfire.com/api/characters?page=${i}&pageSize=4`)
     .then((response)=>response.json())
@@ -22,28 +23,29 @@ for(let i = 1; i < 1070; i++)
         let div = document.createElement('div');
         let outDiv = document.createElement('div')
         outDiv.className = "name column align-items-center justify-content-center m-2"
-
         let newArray = e.map((element)=>{
+            
+            characters.push(element);
             
             if(element.name == '' && element.allegiances.length > 0)
             {
                 let houseArr = element.allegiances[0].split('/')
                 let house = houseArr.pop();
-                return `<div class="col-3 d-inline-flex"><a href="#">${houses[house-1]}</a><br>${element.aliases[0]}</br></div>`
+                return `<div class="col-3 d-inline-flex">${element.aliases[0]}-<a href="#">${houses[house-1]}</a></div>`
             }
             else if(element.name != '' && element.allegiances.length > 0)
             {
                 let houseArr = element.allegiances[0].split('/')
                 let house = houseArr.pop();
-                return `<div class="col-3 d-inline-flex"><a href="#">${houses[house-1]}</a><br>${element.name}</br></div>`;
+                return `<div class="col-3 d-inline-flex">${element.name}-<a href="#">${houses[house-1]}</a></div>`;
             }
             else if(element.name != '' && element.allegiances.length == 0) 
             {
-                return `<div class="col-3 d-inline-flex">No House<br>${element.name}</br></div>`;
+                return `<div class="col-3 d-inline-flex">${element.name}-No House</div>`;
             }
             else 
             {
-                return `<div class="col-3 d-inline-flex">No House<br>${element.aliases[0]}</br></div>`;
+                return `<div class="col-3 d-inline-flex">${element.aliases[0]}-No House</div>`;
             }
         })
 
@@ -52,3 +54,5 @@ for(let i = 1; i < 1070; i++)
         container.append(outDiv)
     })
 }
+
+console.log(characters);
